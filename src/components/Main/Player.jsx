9c2e7158e7b09d73d663034f08a3fbcae790d44b
muath-khatib <mo3az.khatib@hotmail.com>
2021-09-22 17:03:03 +0300

    Set the link of video in local storage Relates #29

--- src/components/Main/Player.jsx
+++ src/components/Main/Player.jsx
@@ -3,14 +3,14 @@ import ReactJWPlayer from "react-jw-player";
 import { Watcher } from "../../context";
 
 const Player = () => {
-  console.log("player", localStorage.getItem("stream"));
+  const local = localStorage.getItem("streaming")
   const { StreamLink } = useContext(Watcher);
   return (
     <section className="player">
       <ReactJWPlayer
         playerId="my-unique-id"
         playerScript="https://cdn.jwplayer.com/libraries/ynxSe3tf.js"
-        file={StreamLink}
+        file={StreamLink ? StreamLink : local}
         isAutoPlay={true}
       />
     </section>
